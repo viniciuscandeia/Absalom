2c36e6baa130af41fef397b2edadc13b163e6230
Vinicius Candeia <viniciuscandeia12@gmail.com>
2024-10-24 11:55:45 -0300

    feat(validador): Criando um validador de informacoes

--- src/entidades/entidade_produto.py
+++ src/entidades/entidade_produto.py
@@ -4,15 +4,15 @@ from .entidade import Entidade
 class Produto(Entidade):
     def __init__(self, id_: int,
                  nome: str,
-                 tipo: str,
+                 descricao: str,
                  preco: float,
                  quantidade: int,
                  id_loja: int):
         super().__init__(id_, nome)
-        self.tipo = tipo
+        self.descricao = descricao
         self.preco = preco
         self.quantidade = quantidade
         self.id_loja = id_loja
 
     def __str__(self):
-        return f"[{self.id_}] Nome: {self.nome}, Tipo: {self.tipo}, Preco: {self.preco}, Quantidade: {self.quantidade}, Loja: {self.id_loja}"
+        return f"[{self.id_}] Nome: {self.nome}, Descrição: {self.descricao}, Preco: {self.preco}, Quantidade: {self.quantidade}, Loja: {self.id_loja}"
