fcaaf5b4972b9b4a4b9626e3ccda2e42114d7efa
João Victor <jvictorm083@gmail.com>
2024-10-27 22:35:29 -0300

    doc:enviando_diagrama_de_classes

--- teste_telas.py
+++
@@ -1,7 +0,0 @@
-from src.telas.telas_listar_usuarios import TelaListarAdministradores
-from src.fabricas.fabrica_gerenciadores_usuarios import FabricaGerenciadorUsuarios
-
-gerenciador = FabricaGerenciadorUsuarios.criar_gerenciador('db')
-lista = gerenciador.listar('administrador')
-
-TelaListarAdministradores.tela(lista)
